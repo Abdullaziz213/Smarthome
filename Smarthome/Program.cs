77b3e579c947ffeaa356a716e9bf1ff4acca3e05
Abdullaziz Hassen Abdella <abdullaziz.hassenabdella@ksb-sg.ch>
2024-02-12 09:27:15 +0100

    Add project files.

---
+++ Smarthome/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Smarthome
+{
+    public interface IZimmer
+    {
+        string Name { get; }
+    }
+
+    public abstract class ZimmerDecorator
+    {
+
+    }
+
+    public class Zimmer
+    {
+
+    }
+
+    public class BadWC
+    {
+
+    }
+
+    public class Kueche
+    {
+
+    }
+
+    public class Wintergarten
+    {
+
+    }
+
+    public class Schlafzimmer
+    {
+
+    }
+
+    public class Wohnzimmer
+    {
+
+    }
+}
